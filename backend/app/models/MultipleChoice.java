7b0143ebadaae4b45aa02588f9fa860f0b659e90
cagdasozbayrak <Pink_floyd902>
2017-12-19 21:41:31 +0300

    Model classes and sql create table scripts done.

---
+++ backend/app/models/MultipleChoice.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import io.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity
+@Table
+public class MultipleChoice extends Question{
+
+    @Constraints.Required
+    public String choices;
+}
--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -2,40 +2,81 @@
 
 # --- !Ups
 
-create table company (
+create table userAccount (
   id                        bigint not null,
-  name                      varchar(255),
-  constraint pk_company primary key (id))
+  email                     varchar(255) not null,
+  passwordHash              varchar(255) not null,
+  passwordSalt              varchar(255) not null,
+  user_id                   bigint not null,
+  constraint pk_user primary key (id))
 ;
 
-create table computer (
-  id                        bigint not null,
-  name                      varchar(255),
-  introduced                timestamp,
-  discontinued              timestamp,
-  company_id                bigint,
-  constraint pk_computer primary key (id))
+CREATE TABLE userprofile (
+    id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    testStatistics VARCHAR,
+    profilePic VARCHAR NOT NULL,
+    bip VARCHAR NOT NULL,
+    PRIMARY KEY (id)
+)
 ;
 
-create sequence company_seq start with 1000;
+CREATE TABLE coding (
+    id BIGINT NOT NULL,
+    questionContent VARCHAR,
+    category INT NOT NULL,
+    difficulty INT NOT NULL,
+    timeToSolve INT NOT NULL,
+    inputs VARCHAR NOT NULL,
+    outputs VARCHAR,
+    programmingLanguage VARCHAR,
+    PRIMARY KEY (id)
+)
+;
 
-create sequence computer_seq start with 1000;
+CREATE TABLE multiplechoice (
+    id BIGINT NOT NULL,
+    questionContent VARCHAR,
+    category INT NOT NULL,
+    difficulty INT NOT NULL,
+    timetosolve INT NOT NULL,
+    answer VARCHAR,
+    PRIMARY KEY (id)
+)
+;
 
-alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
-create index ix_computer_company_1 on computer (company_id);
+create sequence user_seq start with 1000;
+
+create sequence userprofile_seq start with 1000;
+
+create sequence coding_seq start with 1;
+
+create sequence multiplechoice_seq start with 1;
+
+-- Emin değilim buradan
+alter table userAccount add constraint fk_user_userprofile_1 foreign key (user_id) references userprofile (id) on delete restrict on update restrict
+create index ix_user_userprofile_1 on userAccount (user_id);
 
 
 # --- !Downs
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table if exists company;
+drop table if exists userAccount;
 
-drop table if exists computer;
+drop table if exists userprofile;
+
+drop table if exists coding;
+
+drop table if exists multiplechoice;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
-drop sequence if exists company_seq;
+drop sequence if exists user_seq;
 
-drop sequence if exists computer_seq;
+drop sequence if exists userprofile_seq;
+
+drop sequence if exists coding_seq start;
+
+drop sequence if exists multiplechoice_seq;
 
