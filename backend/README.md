a959908130a1845afbf1e71e0006c5747dd750f3
cagdasozbayrak <Pink_floyd902>
2017-12-28 12:41:11 +0300

    backend example project changed yaay

--- backend/README.md
+++ backend/README.md
@@ -1,25 +1,53 @@
-[<img src="https://img.shields.io/travis/playframework/play-java-ebean-example.svg"/>](https://travis-ci.org/playframework/play-java-ebean-example)
+[<img src="https://img.shields.io/travis/playframework/play-java-starter-example.svg"/>](https://travis-ci.org/playframework/play-java-starter-example)
 
-# play-java-ebean-example
+# play-java-starter-example
 
-This is an example Play application that uses Java, and communicates with an in memory database using EBean.
+This is a starter application that shows how Play works.  Please see the documentation at https://www.playframework.com/documentation/latest/Home for more details.
 
-The Github location for this project is:
+## Running
 
-[https://github.com/playframework/play-java-ebean-example](https://github.com/playframework/play-java-ebean-example)
+Run this using [sbt](http://www.scala-sbt.org/).  If you downloaded this project from http://www.playframework.com/download then you'll find a prepackaged version of sbt in the project directory:
 
-## Play
+```
+sbt run
+```
 
-Play documentation is here:
+And then go to http://localhost:9000 to see the running web application.
 
-[https://playframework.com/documentation/latest/Home](https://playframework.com/documentation/latest/Home)
+## Controllers
 
-## EBean
+There are several demonstration files available in this template.
 
-EBean is a Java ORM library that uses SQL:
+- HomeController.java:
 
-[https://www.playframework.com/documentation/latest/JavaEbean](https://www.playframework.com/documentation/latest/JavaEbean)
+  Shows how to handle simple HTTP requests.
 
-and the documentation can be found here:
+- AsyncController.java:
 
-[https://ebean-orm.github.io/](https://ebean-orm.github.io/)
+  Shows how to do asynchronous programming when handling a request.
+
+- CountController.java:
+
+  Shows how to inject a component into a controller and use the component when
+  handling requests.
+
+## Components
+
+- Module.java:
+
+  Shows how to use Guice to bind all the components needed by your application.
+
+- Counter.java:
+
+  An example of a component that contains state, in this case a simple counter.
+
+- ApplicationTimer.java:
+
+  An example of a component that starts when the application starts and stops
+  when the application stops.
+
+## Filters
+
+- ExampleFilter.java
+
+  A simple filter that adds a header to every response.
--- backend/test/FunctionalTest.java
+++ backend/test/FunctionalTest.java
@@ -1,75 +1,24 @@
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
-import play.mvc.Result;
 import play.test.WithApplication;
-
-import java.util.HashMap;
-import java.util.Map;
+import play.twirl.api.Content;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static play.api.test.CSRFTokenHelper.addCSRFToken;
-import static play.test.Helpers.*;
 
-// Use FixMethodOrder to run the tests sequentially
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+/**
+ * A functional test starts a Play application for every test.
+ *
+ * https://www.playframework.com/documentation/latest/JavaFunctionalTest
+ */
 public class FunctionalTest extends WithApplication {
 
     @Test
-    public void redirectHomePage() {
-        Result result = route(app, controllers.routes.HomeController.index());
+    public void renderTemplate() {
+        // If you are calling out to Assets, then you must instantiate an application
+        // because it makes use of assets metadata that is configured from
+        // the application.
 
-        assertThat(result.status()).isEqualTo(SEE_OTHER);
-        assertThat(result.redirectLocation().get()).isEqualTo("/computers");
+        Content html = views.html.index.render("Your new application is ready.");
+        assertThat("text/html").isEqualTo(html.contentType());
+        assertThat(html.body()).contains("Your new application is ready.");
     }
-
-    @Test
-    public void listComputersOnTheFirstPage() {
-        Result result = route(app, controllers.routes.HomeController.list(0, "name", "asc", ""));
-
-        assertThat(result.status()).isEqualTo(OK);
-        assertThat(contentAsString(result)).contains("574 computers found");
-    }
-
-    @Test
-    public void filterComputerByName() {
-        Result result = route(app, controllers.routes.HomeController.list(0, "name", "asc", "Apple"));
-
-        assertThat(result.status()).isEqualTo(OK);
-        assertThat(contentAsString(result)).contains("13 computers found");
-    }
-
-    @Test
-    public void createANewComputer() {
-        Result result = route(app, addCSRFToken(fakeRequest().uri(controllers.routes.HomeController.save().url())));
-        assertThat(result.status()).isEqualTo(OK);
-
-        Map<String, String> data = new HashMap<>();
-        data.put("name", "FooBar");
-        data.put("introduced", "badbadbad");
-        data.put("company.id", "1");
-
-        String saveUrl = controllers.routes.HomeController.save().url();
-        result = route(app, addCSRFToken(fakeRequest().bodyForm(data).method("POST").uri(saveUrl)));
-
-        assertThat(result.status()).isEqualTo(BAD_REQUEST);
-        assertThat(contentAsString(result)).contains("<option value=\"1\" selected=\"selected\">Apple Inc.</option>");
-        //  <input type="text" id="introduced" name="introduced" value="badbadbad" aria-describedby="introduced_info_0 introduced_error_0" aria-invalid="true" class="form-control">
-        assertThat(contentAsString(result)).contains("<input type=\"text\" id=\"introduced\" name=\"introduced\" value=\"badbadbad\" ");
-        // <input type="text" id="name" name="name" value="FooBar" aria-describedby="name_info_0" required="true" class="form-control">
-        assertThat(contentAsString(result)).contains("<input type=\"text\" id=\"name\" name=\"name\" value=\"FooBar\" ");
-
-        data.put("introduced", "2011-12-24");
-
-        result = route(app, fakeRequest().bodyForm(data).method("POST").uri(saveUrl));
-
-        assertThat(result.status()).isEqualTo(SEE_OTHER);
-        assertThat(result.redirectLocation().get()).isEqualTo("/computers");
-        assertThat(result.flash().get("success")).isEqualTo("Computer FooBar has been created");
-
-        result = route(app, controllers.routes.HomeController.list(0, "name", "asc", "FooBar"));
-        assertThat(result.status()).isEqualTo(OK);
-        assertThat(contentAsString(result)).contains("One computer found");
-    }
-
 }
